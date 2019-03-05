4c5c268310b8e0ec318d1c1d284e7316a2a8cc9c
Andrew Sveikauskas <a.l.sveikauskas@gmail.com>
2019-03-05 11:10:00 -0800

    c++/new.h - extra helpers for smart pointer types

--- include/common/c++/new.h
+++ include/common/c++/new.h
@@ -10,7 +10,9 @@
 #define common_cxx_new_h_
 
 #include <new>
+#include <memory>
 #include <common/error.h>
+#include <common/c++/refcount.h>
 
 namespace common {
 
@@ -28,6 +30,53 @@ New(T **ptr, error *err)
       ERROR_SET(err, nomem);
    }
 exit:;
+}
+
+template <typename T>
+void
+New(common::Pointer<T> &ptr, error *err)
+{
+   New(ptr.GetAddressOf(), err);
+}
+
+template <typename T>
+void
+New(std::shared_ptr<T> &ptr, error *err)
+{
+   T *p = nullptr;
+   try
+   {
+      p = new T();
+      ptr = std::shared_ptr<T>(p);
+      p = nullptr;
+   }
+   catch (std::bad_alloc)
+   {
+      ERROR_SET(err, nomem);
+   }
+exit:
+   if (p)
+      delete p;
+}
+
+template <typename T>
+void
+New(std::unique_ptr<T> &ptr, error *err)
+{
+   T *p = nullptr;
+   try
+   {
+      p = new T();
+      ptr = std::unique_ptr<T>(p);
+      p = nullptr;
+   }
+   catch (std::bad_alloc)
+   {
+      ERROR_SET(err, nomem);
+   }
+exit:
+   if (p)
+      delete p;
 }
 
 } // end namespace
