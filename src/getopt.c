5894ce33834728d6a116bff5e3d7b8d4d919321a
Andrew Sveikauskas <a.l.sveikauskas@gmail.com>
2019-11-01 14:25:54 -0700

    Borrowed Berkeley getopt.c: Put copyright msg on single line.
    This is helpful for a script I'm working on that tries to slurp up
    copyright messages from source so that I may properly attribute all
    the open source stuff a project is linking to.

--- src/getopt.c
+++ src/getopt.c
@@ -1,6 +1,6 @@
 /*
-* Copyright (c) 1987, 1993, 1994
-*      The Regents of the University of California.  All rights reserved.
+* Copyright (c) 1987, 1993, 1994 The Regents of the University of California.
+*      All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions
