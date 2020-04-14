6cebaf23c539e3e59f866b385730b5c950281944
Andrew Sveikauskas <a.l.sveikauskas@gmail.com>
2020-04-14 09:29:18 -0700

    size.h: update copyright year

--- include/common/size.h
+++ include/common/size.h
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2017 Andrew Sveikauskas
+ Copyright (C) 2017-2020 Andrew Sveikauskas
 
  Permission to use, copy, modify, and distribute this software for any
  purpose with or without fee is hereby granted, provided that the above
